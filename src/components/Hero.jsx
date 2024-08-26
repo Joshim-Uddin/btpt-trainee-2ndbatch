30a6ef11284b5cc0cc9a8093a6e528138b5171bb
Joshim-Uddin <joshimuddin1831@gmail.com>
2024-08-26 16:06:53 +0600

    hero section modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,32 @@ const images= ["https://res.cloudinary.com/dfxk2ofkn/image/upload/v1723602960/ni
 const Hero = () => {
     return (
         <>
-      <Swiper navigation={true} autoplay={true} modules={[Navigation, Autoplay]} className="mySwiper">
+      {/* <div className='grid grid-cols-3 items-center justify-center'>
+        
+        
+     
+      </div> */}
+      <div className='grid grid-cols-5 justify-between gap-5 items-center'>
+        <div className='col-span-2 relative'>
+          <div className="relative">
+          <div className='one absolute top-10 right-20'></div>
+          <div className='two absolute -bottom-5 left-64'></div>
+          <div className='three absolute bottom-5 right-10'></div>
+          <div className='two absolute -bottom-10 left-0'></div>
+          <div className='three absolute bottom-24 right-48'></div>
+          <div className='two absolute -top-32 left-28'></div>
+          <div className='three absolute top-0 right-0'></div>
+          </div>
+        <h2 className='md:text-5xl text-2xl text-slate-700 font-semibold md:leading-relaxed text-center absolute -top-1/2 left-0 -translate-y-1/2'>Welcome to BTPT BATCH-2 Portal</h2>
+        </div>
+        <Swiper navigation={true} autoplay={true} modules={[Navigation, Autoplay]} className="mySwiper col-span-3 w-full">
         {
-            images.map((image, index)=> <SwiperSlide key={index}><img src={image} alt="image of trainee" className='h-screen w-full'/></SwiperSlide>)
+            images.map((image, index)=> <SwiperSlide key={index}><img src={image} alt="image of trainee"  className='md:h-[calc(100vh-120px)] h-52 w-full'/></SwiperSlide>)
         }
       </Swiper>
+        {/* <img src="https://res.cloudinary.com/dfxk2ofkn/image/upload/v1724030250/two_srx0bg.jpg" alt="" className='w-full' /> */}
+      </div>
+      
     </>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <nav className='text-white bg-violet-700 px-10 min-h-20 py-5'>
-            <div className='flex justify-between items-center'>
+        <nav className='text-white'>
+            <div className='flex justify-between items-center max-[600px]:hidden px-10 min-h-20 py-5 bg-violet-700'>
                 <h2 className='text-3xl text-amber-200'>BTPT BATCH-2</h2>
                 <ul className='flex justify-between items-center gap-5'>
                 <li className='text-xl'> <Link to="/">Home</Link> </li>
@@ -13,6 +15,21 @@ const Navbar = () => {
                 <li className='text-xl'><Link to="/about">About Us</Link></li>
             </ul>
             </div>
+            <div className='flex justify-between items-center min-[600px]:hidden px-10 min-h-20 py-5 bg-violet-700'>
+                <h2 className='text-3xl text-amber-200'>BTPT BATCH-2</h2>
+                <div>
+                <FaBarsStaggered onClick={()=>setOpen(!open)}/>
+                {
+                    open?<ul className='flex-col justify-between items-center gap-5'>
+                    <li className='text-lg'> <Link to="/">Home</Link> </li>
+                    <li className='text-lg'> <Link to="/magazine">Wall Magazine</Link></li>
+                    <li className='text-lg'><Link to="/gallery">Gallery</Link></li>
+                    <li className='text-lg'><Link to="/about">About Us</Link></li>
+                </ul>:""
+                }
+                </div>
+            </div>
+
         </nav>
     );
 };
